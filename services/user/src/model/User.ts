8eb335df8705238a9e99cf97d068bf2c18e68197
shahirsizan <shahir.sizan18@gmail.com>
2025-09-01 19:27:19 +0600

    user model created

---
+++ services/user/src/model/User.ts
@@ -0,0 +1,34 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+	name: string;
+	email: string;
+	image: string;
+	bio: string;
+}
+
+const userSchema: Schema<IUser> = new Schema(
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		image: {
+			type: String,
+			required: true,
+		},
+		bio: String,
+	},
+	{
+		timestamps: true,
+	}
+);
+
+const User = mongoose.model<IUser>("User", userSchema);
+
+export default User;
