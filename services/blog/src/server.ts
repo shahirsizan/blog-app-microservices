093a036d832737ca84d2404a778151e023a46a05
shahirsizan <shahir.sizan18@gmail.com>
2025-09-04 22:29:49 +0600

    server.ts initialized

---
+++ services/blog/src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import "dotenv/config";
+import blogRoutes from "./routes/blog.js";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT;
+
+app.use("/api/v1", blogRoutes);
+
+app.listen(port, () => {
+	console.log(`Server is running on http://localhost:${port}`);
+});
