78b45fb161fdc5d9b6be01420eedd951ceac8fbd
shahirsizan <shahir.sizan18@gmail.com>
2025-09-04 22:21:59 +0600

    blog controller boilerplate code added

---
+++ services/blog/src/controllers/blog.ts
@@ -0,0 +1,62 @@
+export const getAllBlogs = async (req: any, res: any) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const getSingleBlog = async (req: any, res: any) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const addComment = async (req: any, res: any) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const getAllComments = async (req: any, res: any) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const deleteComment = async (req: any, res: any) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const saveBlog = async (req: any, res: any) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const getSavedBlog = async (req: any, res: any) => {
+	try {
+	} catch (error: any) {
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
