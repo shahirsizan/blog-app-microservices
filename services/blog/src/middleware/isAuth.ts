f3239d7d9e86fd4ae7ad7b2a92a8a91a4ed0e4cc
shahirsizan <shahir.sizan18@gmail.com>
2025-09-04 22:23:21 +0600

    isAuth middleware added

---
+++ services/blog/src/middleware/isAuth.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+export const isAuth = async (req: any, res: any, next: any): Promise<void> => {
+	try {
+		const authHeader = req.headers.authorization;
+
+		if (!authHeader) {
+			res.status(401).json({
+				message: "Please Login - No auth header",
+			});
+			return;
+		}
+
+		const token = authHeader.split(" ")[1];
+
+		const decodeValue = jwt.verify(
+			token,
+			process.env.JWT_SEC as string
+		) as JwtPayload;
+
+		if (!decodeValue) {
+			res.status(401).json({
+				message: "Invalid token",
+			});
+			return;
+		}
+
+		// append `user` object to `req` and pass to next()
+		req.user = decodeValue.user;
+		next();
+	} catch (error) {
+		console.log("JWT verification error: ", error);
+		res.status(401).json({
+			message: "Please Login - Jwt error",
+		});
+	}
+};
