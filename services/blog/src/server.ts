cdde204a7737e5063eeae1e13233161c205e11ee
shahirsizan <shahir.sizan18@gmail.com>
2025-09-05 23:22:10 +0600

    Redis client instantiated

--- services/blog/src/server.ts
+++ services/blog/src/server.ts
@@ -2,12 +2,21 @@ import express from "express";
 import "dotenv/config";
 import blogRoutes from "./routes/blog.js";
 import cors from "cors";
+import { createClient } from "redis";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 const port = process.env.PORT;
+
+export const redisClient = createClient({
+	url: process.env.REDIS_URL,
+});
+await redisClient.connect();
+redisClient.on("error", (err) => {
+	console.log("blog -> server.ts: ", err);
+});
+console.log("Connected to redis.");
 
 app.use("/api/v1", blogRoutes);
 
