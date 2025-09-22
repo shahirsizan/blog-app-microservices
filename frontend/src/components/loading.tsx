7971fa52daf2f21ebbf73b7af9a563683df05927
shahirsizan <shahir.sizan18@gmail.com>
2025-09-22 12:09:01 +0600

    Loading page added

---
+++ frontend/src/components/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Loading = () => {
+	return (
+		<div className="w-full h-screen flex items-center justify-center">
+			<p className="text-2xl text-blue-500 font-bold text-center">
+				Loading...
+			</p>
+		</div>
+	);
+};
+
+export default Loading;
