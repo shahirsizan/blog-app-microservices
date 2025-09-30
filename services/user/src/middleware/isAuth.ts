4c49b966eaf9b605f2021fbe35f8a483e1ecbb40
shahirsizan <shahir.sizan18@gmail.com>
2025-09-30 21:24:52 +0600

    comment added

--- services/user/src/middleware/isAuth.ts
+++ services/user/src/middleware/isAuth.ts
@@ -34,6 +34,17 @@ export const isAuth = async (req: any, res: any, next: any): Promise<void> => {
 
 		// append `user` object to `req`, pass to next()
 		req.user = decodedValue.user;
+		// console.log("req.user: ", req.user);
+		// req.user:  {
+		// [1]   _id: '68d033c2c8d3c05f48030914',
+		// [1]   name: 'Shahir Adil Sizan',
+		// [1]   email: 'shahir.sizan18@gmail.com',
+		// [1]   image: 'https://res.cloudinary.com/deh1ctb1t/image/upload/v1758650283/blogs/eo5a8ljllmmqpm5knzfp.png',
+		// [1]   createdAt: '2025-09-21T17:20:02.810Z',
+		// [1]   updatedAt: '2025-09-23T17:58:33.166Z',
+		// [1]   __v: 0,
+		// [1]   bio: 'Software Engineer'
+		// [1] }
 		next();
 	} catch (error) {
 		console.log("⚠️ Bearer token empty - Please Login: ", error);
