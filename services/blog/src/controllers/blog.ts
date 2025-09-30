95afa9ad2c5a1f99dbce42e85d611b9bc926c2cf
shahirsizan <shahir.sizan18@gmail.com>
2025-09-30 21:27:16 +0600

    saveBlog & getSingleBlogForAuthUser controller added

--- services/blog/src/controllers/blog.ts
+++ services/blog/src/controllers/blog.ts
@@ -78,7 +78,9 @@ export const getAllBlogs = async (req: any, res: any) => {
 
 export const getSingleBlog = async (req: any, res: any) => {
 	try {
-		const blogid = req.params.id;
+		console.log("inside getSingleBlog");
+
+		const blogId = req.params.id;
 
 		// REDIS DISABLED
 		// const cachedData = await redisClient.get(`blog:${blogid}`);
@@ -90,8 +92,9 @@ export const getSingleBlog = async (req: any, res: any) => {
 		// }
 
 		// not found in cache
+
+		const blog = await sql`SELECT * FROM blogs WHERE id = ${blogId}`;
 		// fetch from db
-		const blog = await sql`SELECT * FROM blogs WHERE id = ${blogid}`;
 		if (blog.length === 0) {
 			res.status(404).json({
 				message: "no blog with this id",
@@ -99,13 +102,84 @@ export const getSingleBlog = async (req: any, res: any) => {
 			return;
 		}
 
-		// also fetch `authorsData` then append to blogData
+		// now fetch `authorsData` then append to blogData
 		const blogAuthorId = blog[0].author;
 		const { data } = await axios.get(
 			`${process.env.USER_SERVICE_URL}/api/v1/user/${blogAuthorId}`
 		);
 
 		const responseData = { blog: blog[0], author: data };
+
+		// REDIS DISABLED
+		// repopulate cache
+		// await redisClient.set(`blog:${blogid}`, JSON.stringify(responseData), {
+		// 	EX: 3600,
+		// });
+
+		console.log("Blog Served from db");
+		res.json(responseData);
+	} catch (error: any) {
+		console.log("error at getSingleBlog: ", error);
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
+
+export const getSingleBlogForAuthUser = async (req: any, res: any) => {
+	// req.user:  {
+	// [1]   _id: '68d033c2c8d3c05f48030914',
+	// [1]   name: 'Shahir Adil Sizan',
+	// [1]   email: 'shahir.sizan18@gmail.com',
+	// [1]   image: 'https://res.cloudinary.com/deh1ctb1t/image/upload/v1758650283/blogs/eo5a8ljllmmqpm5knzfp.png',
+	// [1]   createdAt: '2025-09-21T17:20:02.810Z',
+	// [1]   updatedAt: '2025-09-23T17:58:33.166Z',
+	// [1]   __v: 0,
+	// [1]   bio: 'Software Engineer'
+	// [1] }
+	try {
+		console.log("inside getSingleBlogForAuthUser");
+
+		const blogId = req.params.id;
+		const userId = req.user._id;
+
+		// REDIS DISABLED
+		// const cachedData = await redisClient.get(`blog:${blogid}`);
+		// if (cachedData) {
+		// 	console.log("Blog Served from Redis cache");
+		// 	const parsedData = JSON.parse(cachedData);
+		// 	res.json(parsedData);
+		// 	return;
+		// }
+
+		// not found in cache
+
+		const blog = await sql`SELECT * FROM blogs WHERE id = ${blogId}`;
+		// fetch from db
+		if (blog.length === 0) {
+			res.status(404).json({
+				message: "no blog with this id",
+			});
+			return;
+		}
+
+		// now fetch whether blog saved by user or not
+		let isSaved = false;
+		const isBlogSaved =
+			await sql`SELECT * FROM savedblogs WHERE blogid = ${blogId} AND userid = ${userId}`;
+		console.log("isBlogSaved: ", isBlogSaved);
+
+		if (isBlogSaved.length > 0) {
+			isSaved = true;
+		}
+
+		// now fetch `authorsData` then append to blogData
+		const blogAuthorId = blog[0].author;
+		const { data } = await axios.get(
+			`${process.env.USER_SERVICE_URL}/api/v1/user/${blogAuthorId}`
+		);
+
+		const responseData = { blog: blog[0], author: data, isSaved: isSaved };
 
 		// REDIS DISABLED
 		// repopulate cache
@@ -182,15 +256,44 @@ export const deleteComment = async (req: any, res: any) => {
 	}
 };
 
-// export const saveBlog = async (req, res) => {
-// 	try {
-// 	} catch (error) {
-// 		console.log("error at saveBlog: ", error);
-// 		res.status(500).json({
-// 			message: error.message,
-// 		});
-// 	}
-// };
+export const saveBlog = async (req: any, res: any) => {
+	try {
+		const { blogid } = req.params;
+		const userid = req.user?._id;
+
+		if (!blogid || !userid) {
+			res.status(400).json({
+				message: "Missing blog id or userid",
+			});
+			return;
+		}
+
+		const alreadySaved =
+			await sql`SELECT * FROM savedblogs WHERE blogid = ${blogid} AND userid = ${userid}`;
+
+		// if already saved, then unsave
+		if (alreadySaved.length !== 0) {
+			await sql`DELETE FROM savedblogs WHERE blogid = ${blogid} AND userid = ${userid}`;
+			res.status(200).json({
+				message: "Blog Unsaved",
+			});
+			return;
+		}
+		// if not saved, then save
+		if (alreadySaved.length === 0) {
+			await sql`INSERT INTO savedblogs (blogid, userid) VALUES (${blogid}, ${userid})`;
+			res.status(200).json({
+				message: "Blog Saved",
+			});
+			return;
+		}
+	} catch (error: any) {
+		console.log("❌ error -> saveBlog(): ", error);
+		res.status(500).json({
+			message: error.message,
+		});
+	}
+};
 
 // export const getSavedBlog = async (req, res) => {
 // 	try {
